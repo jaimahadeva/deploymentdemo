b68dc44c01fa04b0e872d83aabffb655516b9a3f
balaji8080-naginder <balajibiradar8080@gmail.com>
2022-08-07 16:59:50 +0530

    first commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';   
+   @Component({       
+ selector: 'backgroundimage',    
+    templateUrl: './app.component.html',     
+   styleUrls: ['app.component.css']    })   
+  
+  export class BackgroundimageComponent {  
+      constructor() { }   
+   }   
