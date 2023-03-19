6b65018bd796d2a0057f2d686dec179ae1d8438a
mahadeva <mahadevb909@gmail.com>
2023-03-19 17:30:21 +0530

    yuuyu

--- src/app/app.component.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';   
-   @Component({       
- selector: 'backgroundimage',    
-    templateUrl: './app.component.html',     
-   styleUrls: ['app.component.css']    })   
-  
-  export class BackgroundimageComponent {  
-      constructor() { }   
-   }   
